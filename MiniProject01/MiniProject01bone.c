6612da683d7633feb607f122b1f9f481a136c0f0
root <root@beaglebone.(none)>
2012-09-13 04:01:47 +0000

    Finished Last requirement for miniproject

--- MiniProject01/MiniProject01bone.c
+++ MiniProject01/MiniProject01bone.c
@@ -7,6 +7,7 @@
 #include <fcntl.h>
 #include <poll.h>
 #include <signal.h>
+#include "i2c-dev.h"
 
 int loop=1;
 
@@ -30,43 +31,75 @@ int main(int argc, char** argv){
 	int gpio2_value = 0;
 	int pattern =0;
 	int value =0;
-	//check that at least two arguments are passed in
-	if(argc < 3){
-		printf("Usage: %s <input-gpio> <output-gpio>\n", argv[0]);
+	char *end;
+	int res, i2cbus, address, size, file;
+	int daddress;
+	char filename[20];
+	
+
+	//Setting the signal handler for Ctrl + C
+	if (signal(SIGINT, signal_handler) == SIG_ERR)
+		printf("\ncan't catch SIGINT\n");
+
+
+	//Argument checking for at least five
+	if(argc < 6){
+		printf("Usage: %s <input-gpio> <output-gpio> <i2c-bus> <i2c-address> <register>\n", argv[0]);
 		printf("polls input-gpio, and writes value to output-gpio\n");
 		fflush(stdout);
 		return 1;
 	}
 
-	//set signal handler for Ctrl + C
-	if (signal(SIGINT, signal_handler) == SIG_ERR)
-		printf("\ncan't catch SIGINT\n");
-
-	//assign gpio values
+	//Assigning gpio values
 	gpio1 = atoi(argv[1]);
 	gpio2 = atoi(argv[2]);
 
-	//argument 1 will be input
+	//Input for Argument 1
 	export_gpio(gpio1);
 	set_gpio_direction(gpio1, "in");
 	set_gpio_edge(gpio1, "falling");
 	gpio1_fd = gpio_fd_open(gpio1);
 	
-	//argument 2 will be output
+	//Output for Argument 2
 	export_gpio(gpio2);
 	set_gpio_direction(gpio2, "out");
 	set_gpio_value(gpio2, gpio2_value);
 	gpio2_fd = gpio_fd_open(gpio2);
 
+	//Assigning I2C values
+	i2cbus   = atoi(argv[3]);
+	address  = atoi(argv[4]);
+	daddress = atoi(argv[5]);
+	size = I2C_SMBUS_BYTE;
+
+	sprintf(filename, "/dev/i2c-%d", i2cbus);
+	file = open(filename, O_RDWR);
+	if (file<0) {
+		if (errno == ENOENT) {
+			fprintf(stderr, "Error: Could not open file "
+				"/dev/i2c-%d: %s\n", i2cbus, strerror(ENOENT));
+		} else {
+			fprintf(stderr, "Error: Could not open file "
+				"`%s': %s\n", filename, strerror(errno));
+			if (errno == EACCES)
+				fprintf(stderr, "Run as root?\n");
+		}
+		exit(1);
+	}
+
+	if (ioctl(file, I2C_SLAVE, address) < 0) {
+			fprintf(stderr,
+				"Error: Could not set address to 0x%02x: %s\n",
+				address, strerror(errno));
+			return -errno;
+	}
+	
 
 	while(loop){
 		memset((void*)fdset, 0, sizeof(fdset));
 	
 		fdset[0].fd = gpio1_fd;
 		fdset[0].events = POLLPRI;
-
-		//fdset[1].fd = gpio2_fd;
-		//fdset[1].events = POLLPRI;
 
 		rc = poll(fdset, nfds, timeout);
 
@@ -82,7 +115,7 @@ int main(int argc, char** argv){
 			read(fdset[0].fd, buf, MAX_BUF);
 			printf("interrupt value=%c\n", buf[0]);
 			pattern++;
-		if(pattern == 3){
+		if(pattern == 4){
 		pattern = 0;
 		}
 		}			
@@ -90,11 +123,13 @@ int main(int argc, char** argv){
 	
 
 
-
+	printf("Case %d\n",pattern);
+	
 	switch(pattern){
 	
-	// blink led 
-	case 0: if(gpio2_value){
+	// Blink led 
+	case 0:
+		if(gpio2_value){
 		gpio2_value = 0;
 		}
 		else{
@@ -102,19 +137,40 @@ int main(int argc, char** argv){
 		}
 		set_gpio_value(gpio2, gpio2_value);
 	break;
-	//PWM output  	
-	case 1: set_mux_value("gpmc_a2",6);
+	// PWM output for blinking LED by frequency and duty cycle  	
+	case 1:	 
+		set_mux_value("gpmc_a2",6);
 		set_pwm("ehrpwm.1:0",10,25);
 	break;
+	// Analog In for reading Voltage from POT
 	case 2: unset_pwm("ehrpwm.1:0");
 		value = read_ain("ain6");
 		printf("Voltage: %d\n",value);
 	break;	
+	
+	// I2C Device reading temperature from the sensor
+	case 3:
+		printf("Case 3\n");
+		res = i2c_smbus_write_byte(file, daddress);
+		if (res < 0) {
+			fprintf(stderr, "Warning - write failed, filename=%s, daddress=%d\n",
+				filename, daddress);
+		}
+		res = i2c_smbus_read_byte_data(file, daddress);
+		if (res < 0) {
+			fprintf(stderr, "Error: Read failed, res=%d\n", res);
+			exit(2);
+		}
+
+		printf("Temperature: %d°C\n", res);
+		break;
 	default:
 		break;
 
 	}
 	}
+	
+	close(file);
 	gpio_fd_close(gpio1_fd);
 	gpio_fd_close(gpio2_fd);
 	unexport_gpio(gpio1);
--- MiniProject01/ReadMe.txt
+++ MiniProject01/ReadMe.txt
@@ -1,1 +1,23 @@
 Miniproject 01
+MiniProject01bone.c by Peter Ngo
+header.h: credit given to Andrew Miller.
+
+To run the code with ./a.out there needs to be 5 arguments after it.
+Each one is the default values such as the first two arguments are the input for the push button and the output to the LED. While the last 3 are for i2c sensor.
+
+As follows the correct arguments to run the program is:
+
+./a.out 7 60 3 75 0
+
+
+There will be 4 cases all interrupted by the push button to cycle through each one. Each case is displayed on the terminal whenever the case is selected to keep track of which case is currently selected.
+
+Case 0: The gpio output to show a blinking LED which toggles on and off after a certain time in milliseconds.
+
+Case 1: This is the PWM output also used to show the flashing of an LED with the added values that the rate of the flashing of the LED is shown by the frequency and duty cycle given in the said case statement.
+
+Case 2: This is the Analog in where the voltage from the potentiometer is read and displayed in the terminal when ran and changes whenever the pot is turned.
+
+Case 3: This is the i2c device where the Temperature is read in Celsius and displayed in the terminal and also changes whenever the sensor detects a change in the temperature.
+
+All these cases are placed in an infinite while loop which can be exited out with an interrupt from Ctrl + C.
